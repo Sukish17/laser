bebdc23aa4de0bb69f03be971c5408500dc27149
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-11 21:23:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 
 
-mongoose.connect("mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
@@ -26,7 +26,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl:"mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        mongoUrl:"mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0"
     }),
     cookie: { maxAge: 24 * 60 * 60 * 1000 } // 1 day
 }));
